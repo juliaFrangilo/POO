6d5779d885265cd88db3055c7c6af39361f1c4cf
juliaFrangilo <64858376+juliaFrangilo@users.noreply.github.com>
2023-05-24 14:09:27 -0300

    classes

--- Gerenciador/src/modelo/Filial.java
+++ Gerenciador/src/modelo/Filial.java
@@ -1,5 +1,46 @@
 package modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Filial {
 	
+	@Id @GeneratedValue
+	private Long id;
+	
+	private String nome;
+	private String cnpj;
+	
+	@OneToOne
+	private Endereco endereco;
+	
+	public Filial() {}
+
+	public String getNome() {
+		return nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 }
